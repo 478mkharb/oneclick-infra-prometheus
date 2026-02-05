2065e94dc8f8d772dc4c9da84c7090cd920604d0
Mukesh Kharb <478mkharb@gmail.com>
2026-02-06 01:10:30 +0530

    removed iam.tf

--- oneclick-monitoring-NAT/terraform/outputs.tf
+++ oneclick-monitoring-NAT/terraform/outputs.tf
@@ -2,7 +2,3 @@ output "k3s_server_private_ip" {
   value = aws_instance.k3s_server.private_ip
 }
 
-output "k3s_token" {
-  value     = file("/var/lib/rancher/k3s/server/node-token")
-  sensitive = true
-}
