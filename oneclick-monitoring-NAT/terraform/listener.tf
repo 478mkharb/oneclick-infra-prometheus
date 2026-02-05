0a1d8e841be5d2d9c806231fe5505bb934f09295
Mukesh Kharb <478mkharb@gmail.com>
2026-02-05 15:46:11 +0530

    removed iam.tf

--- oneclick-monitoring-NAT/terraform/listener.tf
+++ oneclick-monitoring-NAT/terraform/listener.tf
@@ -8,18 +8,13 @@ resource "aws_lb_listener" "http" {
     target_group_arn = aws_lb_target_group.grafana.arn
   }
 }
-resource "aws_lb_listener_rule" "prometheus" {
-  listener_arn = aws_lb_listener.http.arn
-  priority     = 10
+resource "aws_lb_listener" "prometheus" {
+  load_balancer_arn = aws_lb.monitoring.arn
+  port              = 9090
+  protocol          = "HTTP"
 
-  action {
+  default_action {
     type             = "forward"
     target_group_arn = aws_lb_target_group.prometheus.arn
-  }
-
-  condition {
-    path_pattern {
-      values = ["/prometheus*", "/api/v1/*"]
-    }
   }
 }
