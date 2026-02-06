68430251a3b1997c3a3df5d9e3d58da69e91dae9
Mukesh Kharb <478mkharb@gmail.com>
2026-02-06 12:49:08 +0530

    removed iam.tf

--- oneclick-monitoring-NAT/terraform/network_interface.tf
+++ oneclick-monitoring-NAT/terraform/network_interface.tf
@@ -1,7 +1,7 @@
 resource "aws_network_interface" "k3s_server_eni" {
   subnet_id = aws_subnet.private_a.id
 
-  private_ips = ["10.0.3.10"]  # must be inside subnet CIDR
+  private_ips = ["10.0.3.10"]
 
   security_groups = [
     aws_security_group.private_ec2_sg.id
