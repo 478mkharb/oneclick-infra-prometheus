375922b99e853621606ce31db973140abb7446dc
Mukesh Kharb <478mkharb@gmail.com>
2026-02-06 16:23:32 +0530

    removed iam.tf

--- oneclick-monitoring-NAT/terraform/ec2_master.tf
+++ oneclick-monitoring-NAT/terraform/ec2_master.tf
@@ -4,7 +4,8 @@ resource "aws_instance" "k3s_server" {
   iam_instance_profile = "ec2-ssm-profile"
   subnet_id     = aws_subnet.private_a.id
   private_ip = "10.0.3.10"
-
+  security_groups = [aws_security_group.private_ec2_sg.id]
+  
  user_data = base64encode(<<EOF
 #!/bin/bash
 set -ux
