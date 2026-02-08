3fe3a617a9d917c995d8e40bfefdd499d07a7819
Mukesh Kharb <478mkharb@gmail.com>
2026-02-09 05:17:14 +0530

    updated alb.tf

--- oneclick-monitoring-Thanos/terraform/ec2_master.tf
+++ oneclick-monitoring-Thanos/terraform/ec2_master.tf
@@ -1,6 +1,6 @@
 resource "aws_instance" "k3s_server" {
   ami           = data.aws_ami.ubuntu.id
-  instance_type = "c7i-flex.large"
+  instance_type = "t3.small"
   iam_instance_profile = "ec2-ssm-profile"
   subnet_id     = aws_subnet.private_a.id
   private_ip = "10.0.3.10"
