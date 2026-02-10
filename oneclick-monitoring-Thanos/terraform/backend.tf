8be9ae7f7e0d2b23d18099b1aae41c7d9ba164ec
Mukesh Kharb <478mkharb@gmail.com>
2026-02-10 11:29:54 +0530

    Updated backend.tf

--- oneclick-monitoring-Thanos/terraform/backend.tf
+++ oneclick-monitoring-Thanos/terraform/backend.tf
@@ -1,9 +1,9 @@
 terraform {
   backend "s3" {
-    bucket         = "oneclick-terraform.tf-nat"
+    bucket         = "oneclick-monitoring-bucket"
     key            = "oneclick-monitoring/terraform.tfstate"
     region         = "ap-south-1"
     encrypt        = true
-    dynamodb_table = "terraform-locks-nat"
+    dynamodb_table = "terraform-locks-monitoring"
   }
 }
