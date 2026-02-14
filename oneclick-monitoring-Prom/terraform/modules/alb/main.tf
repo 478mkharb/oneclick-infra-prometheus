2c76a5369c370a13b89a09f804494f583c319bb4
Mukesh Kharb <478mkharb@gmail.com>
2026-02-14 18:51:17 +0530

    uploading

--- oneclick-monitoring-Prom/terraform/modules/alb/main.tf
+++ oneclick-monitoring-Prom/terraform/modules/alb/main.tf
@@ -68,17 +68,6 @@ resource "aws_lb_listener" "http" {
   }
 }
 
-resource "aws_lb_listener" "http_grafana" {
-  load_balancer_arn = aws_lb.monitoring.arn
-  port              = 80
-  protocol          = "HTTP"
-
-  default_action {
-    type             = "forward"
-    target_group_arn = aws_lb_target_group.grafana.arn
-  }
-}
-
 resource "aws_lb_listener_rule" "prometheus" {
   listener_arn = aws_lb_listener.http.arn
   priority     = 10
