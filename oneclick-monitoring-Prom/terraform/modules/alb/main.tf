37ae60c4c5a0597327d2e979bb5a2c3bffce2ce4
Mukesh Kharb <478mkharb@gmail.com>
2026-02-14 21:18:14 +0530

    jinja2

--- oneclick-monitoring-Prom/terraform/modules/alb/main.tf
+++ oneclick-monitoring-Prom/terraform/modules/alb/main.tf
@@ -1,6 +1,3 @@
-################################
-# ALB
-################################
 resource "aws_lb" "monitoring" {
   name               = "monitoring-alb"
   internal           = false
@@ -14,29 +11,27 @@ resource "aws_lb" "monitoring" {
   }
 }
 
-################################
-# Grafana Target Group
-################################
 resource "aws_lb_target_group" "grafana" {
   name        = "grafana-tg"
-  port        = 32000
+  port        = 3000
   protocol    = "HTTP"
   vpc_id      = var.vpc_id
   target_type = "instance"
 
   health_check {
-    path                = "/api/health"
+    path                = "/grafana/api/health"
     matcher             = "200"
     interval            = 30
     timeout             = 5
     healthy_threshold   = 2
     unhealthy_threshold = 2
   }
+
+  tags = {
+    Name = "grafana-tg"
+  }
 }
 
-################################
-# Prometheus Target Group
-################################
 resource "aws_lb_target_group" "prometheus" {
   name        = "prometheus-tg"
   port        = 9090
@@ -52,17 +47,39 @@ resource "aws_lb_target_group" "prometheus" {
     healthy_threshold   = 2
     unhealthy_threshold = 2
   }
+
+  tags = {
+    Name = "prometheus-tg"
+  }
 }
 
-################################
-# Listeners
-################################
 resource "aws_lb_listener" "http" {
   load_balancer_arn = aws_lb.monitoring.arn
   port              = 80
   protocol          = "HTTP"
 
   default_action {
+    type = "fixed-response"
+
+    fixed_response {
+      content_type = "text/plain"
+      message_body = "OK"
+      status_code  = "200"
+    }
+  }
+}
+
+resource "aws_lb_listener_rule" "grafana" {
+  listener_arn = aws_lb_listener.http.arn
+  priority     = 20
+
+  condition {
+    path_pattern {
+      values = ["/grafana/*"]
+    }
+  }
+
+  action {
     type             = "forward"
     target_group_arn = aws_lb_target_group.grafana.arn
   }
@@ -74,7 +91,7 @@ resource "aws_lb_listener_rule" "prometheus" {
 
   condition {
     path_pattern {
-      values = ["/prometheus*"]
+      values = ["/prometheus/*"]
     }
   }
 
