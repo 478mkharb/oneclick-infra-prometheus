040b75ff669fe785dba3a816b131812fbc999517
Mukesh Kharb <478mkharb@gmail.com>
2026-02-14 15:54:53 +0530

    uploading

--- oneclick-monitoring-Prom/terraform/modules/security-groups/main.tf
+++ oneclick-monitoring-Prom/terraform/modules/security-groups/main.tf
@@ -3,8 +3,7 @@ resource "aws_security_group" "alb_sg" {
   description = "ALB security group"
   vpc_id      = var.vpc_id
 
-
-  # Grafana
+  # Grafana (via ALB)
   ingress {
     from_port   = 80
     to_port     = 80
@@ -12,7 +11,7 @@ resource "aws_security_group" "alb_sg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
-  # Thanos Query
+  # Prometheus (via ALB :9090)
   ingress {
     from_port   = 9090
     to_port     = 9090
@@ -32,10 +31,9 @@ resource "aws_security_group" "alb_sg" {
     Project = var.project
   }
 }
-
 resource "aws_security_group" "private_ec2_sg" {
   name        = "private-ec2-sg"
-  description = "Security group for private k3s EC2 instances"
+  description = "Security group for monitoring EC2 instances"
   vpc_id      = var.vpc_id
 
   egress {
@@ -50,8 +48,6 @@ resource "aws_security_group" "private_ec2_sg" {
     Project = var.project
   }
 }
-
-# ALB → Grafana
 resource "aws_security_group_rule" "alb_to_grafana" {
   type                     = "ingress"
   from_port                = 32000
@@ -60,58 +56,22 @@ resource "aws_security_group_rule" "alb_to_grafana" {
   security_group_id        = aws_security_group.private_ec2_sg.id
   source_security_group_id = aws_security_group.alb_sg.id
 }
-
-# ALB → Thanos Query (Prometheus HA UI)
-resource "aws_security_group_rule" "alb_to_thanos" {
+resource "aws_security_group_rule" "alb_to_prometheus" {
   type                     = "ingress"
-  from_port                = 30900
-  to_port                  = 30900
+  from_port                = 9090
+  to_port                  = 9090
   protocol                 = "tcp"
   security_group_id        = aws_security_group.private_ec2_sg.id
   source_security_group_id = aws_security_group.alb_sg.id
 }
-
-# k3s API
-resource "aws_security_group_rule" "k3s_api" {
+resource "aws_security_group_rule" "node_exporter" {
   type                     = "ingress"
-  from_port                = 6443
-  to_port                  = 6443
+  from_port                = 9100
+  to_port                  = 9100
   protocol                 = "tcp"
   security_group_id        = aws_security_group.private_ec2_sg.id
   source_security_group_id = aws_security_group.private_ec2_sg.id
 }
-
-# flannel VXLAN
-resource "aws_security_group_rule" "flannel_vxlan" {
-  type                     = "ingress"
-  from_port                = 8472
-  to_port                  = 8472
-  protocol                 = "udp"
-  security_group_id        = aws_security_group.private_ec2_sg.id
-  source_security_group_id = aws_security_group.private_ec2_sg.id
-}
-
-# k3s supervisor
-resource "aws_security_group_rule" "k3s_supervisor" {
-  type                     = "ingress"
-  from_port                = 6444
-  to_port                  = 6444
-  protocol                 = "tcp"
-  security_group_id        = aws_security_group.private_ec2_sg.id
-  source_security_group_id = aws_security_group.private_ec2_sg.id
-}
-
-# kubelet
-resource "aws_security_group_rule" "kubelet" {
-  type                     = "ingress"
-  from_port                = 10250
-  to_port                  = 10250
-  protocol                 = "tcp"
-  security_group_id        = aws_security_group.private_ec2_sg.id
-  source_security_group_id = aws_security_group.private_ec2_sg.id
-}
-
-# CoreDNS UDP
 resource "aws_security_group_rule" "dns_udp" {
   type                     = "ingress"
   from_port                = 53
@@ -121,20 +81,10 @@ resource "aws_security_group_rule" "dns_udp" {
   source_security_group_id = aws_security_group.private_ec2_sg.id
 }
 
-# CoreDNS TCP
 resource "aws_security_group_rule" "dns_tcp" {
   type                     = "ingress"
   from_port                = 53
   to_port                  = 53
-  protocol                 = "tcp"
-  security_group_id        = aws_security_group.private_ec2_sg.id
-  source_security_group_id = aws_security_group.private_ec2_sg.id
-}
-# Node Exporter
-resource "aws_security_group_rule" "node_exporter" {
-  type                     = "ingress"
-  from_port                = 9100
-  to_port                  = 9100
   protocol                 = "tcp"
   security_group_id        = aws_security_group.private_ec2_sg.id
   source_security_group_id = aws_security_group.private_ec2_sg.id
