b00517f48a6e64ec294f60d94adbefca2918f2a9
Mukesh Kharb <478mkharb@gmail.com>
2026-02-14 22:04:21 +0530

    jinja2

--- oneclick-monitoring-Prom/terraform/main.tf
+++ oneclick-monitoring-Prom/terraform/main.tf
@@ -11,27 +11,23 @@ module "security_groups" {
 }
 
 module "alb" {
-  source = "./modules/alb"
-
-  vpc_id                  = module.vpc.vpc_id
-  alb_sg_id               = module.security_groups.alb_sg_id
-  public_subnet_ids       = module.vpc.public_subnet_ids
-  monitoring_instance_id  = module.compute.monitoring_instance_id
-  project                 = var.project
+  source            = "./modules/alb"
+  project           = var.project
+  vpc_id            = module.vpc.vpc_id
+  public_subnet_ids = module.vpc.public_subnet_ids
+  alb_sg_id         = module.security_groups.alb_sg_id
 }
 
 module "compute" {
-  source = "./modules/compute"
-
-  private_subnet_ids  = module.vpc.private_subnet_ids
-  private_ec2_sg_id   = module.security_groups.private_ec2_sg_id
-  role                = var.role   
-  project             = var.project
-
-  grafana_tg_arn      = module.alb.grafana_tg_arn
-  prometheus_tg_arn   = module.alb.prometheus_tg_arn
+  source                   = "./modules/compute"
+  project                  = var.project
+  role                     = var.role
+  instance_type_monitoring = var.instance_type_monitoring
+  private_subnet_ids       = module.vpc.private_subnet_ids
+  private_ec2_sg_id        = module.security_groups.private_ec2_sg_id
+  grafana_tg_arn           = module.alb.grafana_tg_arn
+  prometheus_tg_arn        = module.alb.prometheus_tg_arn
 }
-
 
 module "nacl" {
   source             = "./modules/nacl"
