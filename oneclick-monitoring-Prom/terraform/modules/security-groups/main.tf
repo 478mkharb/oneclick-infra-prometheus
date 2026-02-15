542bbb247bc6c8c68679c328da067789e892d104
Mukesh Kharb <478mkharb@gmail.com>
2026-02-15 17:10:44 +0530

    jinja2

--- oneclick-monitoring-Prom/terraform/modules/security-groups/main.tf
+++ oneclick-monitoring-Prom/terraform/modules/security-groups/main.tf
@@ -76,3 +76,13 @@ resource "aws_security_group_rule" "node_exporter" {
   security_group_id = aws_security_group.private_ec2_sg.id
   self              = true
 }
+
+# Nginx Web App from ALB
+resource "aws_security_group_rule" "alb_to_nginx" {
+  type                     = "ingress"
+  from_port                = 80
+  to_port                  = 80
+  protocol                 = "tcp"
+  security_group_id        = aws_security_group.private_ec2_sg.id
+  source_security_group_id = aws_security_group.alb_sg.id
+}
