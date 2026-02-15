1a857819cfa99c87098bddaecdefa74fc701d47b
Mukesh Kharb <478mkharb@gmail.com>
2026-02-15 08:30:27 +0530

    jinja2

--- oneclick-monitoring-Prom/terraform/modules/security-groups/main.tf
+++ oneclick-monitoring-Prom/terraform/modules/security-groups/main.tf
@@ -3,7 +3,7 @@ resource "aws_security_group" "alb_sg" {
   description = "ALB security group"
   vpc_id      = var.vpc_id
 
-  # Grafana (via ALB)
+  # Public HTTP access
   ingress {
     from_port   = 80
     to_port     = 80
@@ -11,14 +11,7 @@ resource "aws_security_group" "alb_sg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
-  # Prometheus (via ALB :9090)
-  ingress {
-    from_port   = 9090
-    to_port     = 9090
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
+  # ALB to targets (egress only)
   egress {
     from_port   = 0
     to_port     = 0
@@ -33,7 +26,7 @@ resource "aws_security_group" "alb_sg" {
 }
 resource "aws_security_group" "private_ec2_sg" {
   name        = "private-ec2-sg"
-  description = "Security group for monitoring EC2 instances"
+  description = "Security group for private k3s EC2 instances"
   vpc_id      = var.vpc_id
 
   egress {
@@ -48,44 +41,60 @@ resource "aws_security_group" "private_ec2_sg" {
     Project = var.project
   }
 }
-resource "aws_security_group_rule" "alb_to_grafana" {
-  type                     = "ingress"
-  from_port                = 32000
-  to_port                  = 32000
-  protocol                 = "tcp"
-  security_group_id        = aws_security_group.private_ec2_sg.id
-  source_security_group_id = aws_security_group.alb_sg.id
-}
-resource "aws_security_group_rule" "alb_to_prometheus" {
-  type                     = "ingress"
-  from_port                = 9090
-  to_port                  = 9090
-  protocol                 = "tcp"
-  security_group_id        = aws_security_group.private_ec2_sg.id
-  source_security_group_id = aws_security_group.alb_sg.id
-}
-resource "aws_security_group_rule" "node_exporter" {
-  type                     = "ingress"
-  from_port                = 9100
-  to_port                  = 9100
-  protocol                 = "tcp"
-  security_group_id        = aws_security_group.private_ec2_sg.id
-  source_security_group_id = aws_security_group.private_ec2_sg.id
-}
-resource "aws_security_group_rule" "dns_udp" {
-  type                     = "ingress"
-  from_port                = 53
-  to_port                  = 53
-  protocol                 = "udp"
-  security_group_id        = aws_security_group.private_ec2_sg.id
-  source_security_group_id = aws_security_group.private_ec2_sg.id
-}
+resource "aws_security_group" "private_ec2_sg" {
+  name        = "private-ec2-sg"
+  description = "Security group for monitoring EC2 instances"
+  vpc_id      = var.vpc_id
 
-resource "aws_security_group_rule" "dns_tcp" {
-  type                     = "ingress"
-  from_port                = 53
-  to_port                  = 53
-  protocol                 = "tcp"
-  security_group_id        = aws_security_group.private_ec2_sg.id
-  source_security_group_id = aws_security_group.private_ec2_sg.id
+  # Grafana from ALB
+  ingress {
+    from_port                = 3000
+    to_port                  = 3000
+    protocol                 = "tcp"
+    source_security_group_id = aws_security_group.alb_sg.id
+  }
+
+  # Prometheus from ALB
+  ingress {
+    from_port                = 9090
+    to_port                  = 9090
+    protocol                 = "tcp"
+    source_security_group_id = aws_security_group.alb_sg.id
+  }
+
+  # Node exporter (internal)
+  ingress {
+    from_port                = 9100
+    to_port                  = 9100
+    protocol                 = "tcp"
+    source_security_group_id = aws_security_group.private_ec2_sg.id
+  }
+
+  # DNS (internal)
+  ingress {
+    from_port                = 53
+    to_port                  = 53
+    protocol                 = "udp"
+    source_security_group_id = aws_security_group.private_ec2_sg.id
+  }
+
+  ingress {
+    from_port                = 53
+    to_port                  = 53
+    protocol                 = "tcp"
+    source_security_group_id = aws_security_group.private_ec2_sg.id
+  }
+
+  # Outbound
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name    = "private-ec2-sg"
+    Project = var.project
+  }
 }
