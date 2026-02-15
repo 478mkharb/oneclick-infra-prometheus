2c4de9cd270c94201b10a78a9ae52cec9d453e92
Mukesh Kharb <478mkharb@gmail.com>
2026-02-15 08:52:51 +0530

    jinja2

--- oneclick-monitoring-Prom/terraform/modules/nacl/nacl.tf
+++ oneclick-monitoring-Prom/terraform/modules/nacl/nacl.tf
@@ -1,13 +1,15 @@
 resource "aws_network_acl" "private_nacl" {
   vpc_id = var.vpc_id
-  
+
   tags = {
     Name    = "private-nacl"
     Project = var.project
   }
 }
 
+############################################
 # Associate NACL with private subnets
+############################################
 resource "aws_network_acl_association" "private" {
   count = length(var.private_subnet_ids)
 
@@ -15,15 +17,38 @@ resource "aws_network_acl_association" "private" {
   network_acl_id = aws_network_acl.private_nacl.id
 }
 
-
-# --------------------------------------------------
+############################################
 # INBOUND RULES
-# --------------------------------------------------
+############################################
 
-# Node Exporter (Prometheus scraping)
-resource "aws_network_acl_rule" "inbound_node_exporter" {
+# Grafana (ALB → EC2)
+resource "aws_network_acl_rule" "inbound_grafana" {
+  network_acl_id = aws_network_acl.private_nacl.id
+  rule_number    = 80
+  egress         = false
+  protocol       = "tcp"
+  rule_action    = "allow"
+  cidr_block     = var.vpc_cidr
+  from_port      = 3000
+  to_port        = 3000
+}
+
+# Prometheus (ALB → EC2)
+resource "aws_network_acl_rule" "inbound_prometheus" {
   network_acl_id = aws_network_acl.private_nacl.id
   rule_number    = 85
+  egress         = false
+  protocol       = "tcp"
+  rule_action    = "allow"
+  cidr_block     = var.vpc_cidr
+  from_port      = 9090
+  to_port        = 9090
+}
+
+# Node Exporter (internal)
+resource "aws_network_acl_rule" "inbound_node_exporter" {
+  network_acl_id = aws_network_acl.private_nacl.id
+  rule_number    = 90
   egress         = false
   protocol       = "tcp"
   rule_action    = "allow"
@@ -32,10 +57,10 @@ resource "aws_network_acl_rule" "inbound_node_exporter" {
   to_port        = 9100
 }
 
-# Ephemeral TCP (node-to-node, flannel, kubelet, responses)
+# Ephemeral TCP (responses, health checks)
 resource "aws_network_acl_rule" "inbound_ephemeral_tcp" {
   network_acl_id = aws_network_acl.private_nacl.id
-  rule_number    = 90
+  rule_number    = 100
   egress         = false
   protocol       = "tcp"
   rule_action    = "allow"
@@ -44,10 +69,10 @@ resource "aws_network_acl_rule" "inbound_ephemeral_tcp" {
   to_port        = 65535
 }
 
-# Ephemeral UDP (flannel / CNI)
+# Ephemeral UDP (DNS, system traffic)
 resource "aws_network_acl_rule" "inbound_ephemeral_udp" {
   network_acl_id = aws_network_acl.private_nacl.id
-  rule_number    = 95
+  rule_number    = 110
   egress         = false
   protocol       = "udp"
   rule_action    = "allow"
@@ -56,34 +81,10 @@ resource "aws_network_acl_rule" "inbound_ephemeral_udp" {
   to_port        = 65535
 }
 
-# Grafana NodePort
-resource "aws_network_acl_rule" "inbound_grafana" {
-  network_acl_id = aws_network_acl.private_nacl.id
-  rule_number    = 100
-  egress         = false
-  protocol       = "tcp"
-  rule_action    = "allow"
-  cidr_block     = var.vpc_cidr
-  from_port      = 32000
-  to_port        = 32000
-}
-# Prometheus NodePort
-resource "aws_network_acl_rule" "prometheus_nodeport" {
-  network_acl_id = aws_network_acl.private_nacl.id
-  rule_number    = 105
-  egress         = false
-  protocol       = "tcp"
-  rule_action    = "allow"
-  cidr_block     = var.vpc_cidr
-  from_port      = 30900
-  to_port        = 30900
-}
+############################################
+# OUTBOUND RULES (REQUIRED – NACL is stateless)
+############################################
 
-# --------------------------------------------------
-# OUTBOUND RULES
-# --------------------------------------------------
-
-# Allow all outbound traffic (required for stateless NACLs)
 resource "aws_network_acl_rule" "outbound_all" {
   network_acl_id = aws_network_acl.private_nacl.id
   rule_number    = 100
