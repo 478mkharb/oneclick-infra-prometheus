112ae19fa03ab8180cb0d391f8710a503122a667
Mukesh Kharb <478mkharb@gmail.com>
2026-02-15 10:59:53 +0530

    jinja2

--- oneclick-monitoring-Prom/terraform/modules/alb/outputs.tf
+++ oneclick-monitoring-Prom/terraform/modules/alb/outputs.tf
@@ -5,6 +5,7 @@ output "grafana_tg_arn" {
 output "prometheus_tg_arn" {
   value = aws_lb_target_group.prometheus.arn
 }
-output "alb_dns_name" {
+
+output "alb_dns" {
   value = aws_lb.monitoring.dns_name
 }
