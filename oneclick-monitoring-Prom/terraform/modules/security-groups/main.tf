712eb2d77b19567479bebb4c89d1bed454ea9c5c
Mukesh Kharb <478mkharb@gmail.com>
2026-02-15 08:37:48 +0530

    jinja2

--- oneclick-monitoring-Prom/terraform/modules/security-groups/main.tf
+++ oneclick-monitoring-Prom/terraform/modules/security-groups/main.tf
@@ -1,18 +1,23 @@
+############################################
+# ALB Security Group
+############################################
 resource "aws_security_group" "alb_sg" {
   name        = "monitoring-alb-sg"
   description = "ALB security group"
   vpc_id      = var.vpc_id
 
-  # Public HTTP access
+  # Public HTTP access to ALB
   ingress {
+    description = "Allow HTTP from internet"
     from_port   = 80
     to_port     = 80
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
-  # ALB to targets (egress only)
+  # ALB -> EC2 targets
   egress {
+    description = "Allow outbound to targets"
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
@@ -24,23 +29,10 @@ resource "aws_security_group" "alb_sg" {
     Project = var.project
   }
 }
-resource "aws_security_group" "private_ec2_sg" {
-  name        = "private-ec2-sg"
-  description = "Security group for private k3s EC2 instances"
-  vpc_id      = var.vpc_id
 
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  tags = {
-    Name    = "private-ec2-sg"
-    Project = var.project
-  }
-}
+############################################
+# Private EC2 / ASG Security Group
+############################################
 resource "aws_security_group" "private_ec2_sg" {
   name        = "private-ec2-sg"
   description = "Security group for monitoring EC2 instances"
@@ -48,45 +40,51 @@ resource "aws_security_group" "private_ec2_sg" {
 
   # Grafana from ALB
   ingress {
-    from_port                = 3000
-    to_port                  = 3000
-    protocol                 = "tcp"
+    description             = "Grafana from ALB"
+    from_port               = 3000
+    to_port                 = 3000
+    protocol                = "tcp"
     source_security_group_id = aws_security_group.alb_sg.id
   }
 
   # Prometheus from ALB
   ingress {
-    from_port                = 9090
-    to_port                  = 9090
-    protocol                 = "tcp"
+    description             = "Prometheus from ALB"
+    from_port               = 9090
+    to_port                 = 9090
+    protocol                = "tcp"
     source_security_group_id = aws_security_group.alb_sg.id
   }
 
-  # Node exporter (internal)
+  # Node Exporter (internal only)
   ingress {
-    from_port                = 9100
-    to_port                  = 9100
-    protocol                 = "tcp"
+    description             = "Node exporter internal"
+    from_port               = 9100
+    to_port                 = 9100
+    protocol                = "tcp"
     source_security_group_id = aws_security_group.private_ec2_sg.id
   }
 
   # DNS (internal)
   ingress {
-    from_port                = 53
-    to_port                  = 53
-    protocol                 = "udp"
+    description             = "DNS UDP internal"
+    from_port               = 53
+    to_port                 = 53
+    protocol                = "udp"
     source_security_group_id = aws_security_group.private_ec2_sg.id
   }
 
   ingress {
-    from_port                = 53
-    to_port                  = 53
-    protocol                 = "tcp"
+    description             = "DNS TCP internal"
+    from_port               = 53
+    to_port                 = 53
+    protocol                = "tcp"
     source_security_group_id = aws_security_group.private_ec2_sg.id
   }
 
-  # Outbound
+  # Outbound access (SSM, apt, yum, etc.)
   egress {
+    description = "Allow all outbound"
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
