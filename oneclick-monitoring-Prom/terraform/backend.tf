4c44d6655514bfbbb79f2c5f039295b29c9b3be7
Mukesh Kharb <108737400+478mkharb@users.noreply.github.com>
2026-07-03 21:13:47 +0530

    Update backend.tf

--- oneclick-monitoring-Prom/terraform/backend.tf
+++ oneclick-monitoring-Prom/terraform/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket         = "rahul-monitoring-tfstate-860217763718"
-    key            = "oneclick-monitoring/terraform.tfstate"
+    key            = "rahul-monitoring-tfstate-860217763718/terraform.tfstate"
     region         = "ap-south-1"
     encrypt        = true
     dynamodb_table = "terraform-locks-monitoring"
