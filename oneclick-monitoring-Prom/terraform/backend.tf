69e8d153b5466fa8cd4603e4a7ccb9d550804087
Mukesh Kharb <108737400+478mkharb@users.noreply.github.com>
2026-07-03 21:11:55 +0530

    Update S3 bucket name for Terraform backend

--- oneclick-monitoring-Prom/terraform/backend.tf
+++ oneclick-monitoring-Prom/terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "oneclick-monitoring-bucket-rahul"
+    bucket         = "rahul-monitoring-tfstate-860217763718"
     key            = "oneclick-monitoring/terraform.tfstate"
     region         = "ap-south-1"
     encrypt        = true
